72c6ff74121abcb1f3212bc6b622c80222516c29
Damien Stanton <damien@blinkanalytics.com>
2016-06-22 14:49:54 -0400

    cleanup

--- resources/public/js/compiled/devcards_out/reagent/debug.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.9.88 {}
-goog.provide('reagent.debug');
-goog.require('cljs.core');
-
-//# sourceMappingURL=debug.js.map?rel=1466621239119
--- resources/public/js/compiled/devcards_out/reagent/interop.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.9.88 {}
-goog.provide('reagent.interop');
-goog.require('cljs.core');
-
-//# sourceMappingURL=interop.js.map?rel=1466621239111
